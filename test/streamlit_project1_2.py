da2e608323e510d84c3623f7c58c50c859a49dea
LongNguyen81-bidv <121849839+LongNguyen81-bidv@users.noreply.github.com>
2023-04-10 22:14:44 +0700

    Add files via upload

---
+++ test/streamlit_project1_2.py
@@ -0,0 +1,137 @@
+# import libraries
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import underthesea as uts
+# from wordcloud import WordCloud,STOPWORDS
+# import pandas_profiling
+# import scikit-learn
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
+import glob
+import pickle
+
+# Part 1: Build project
+
+##LOAD EMOJICON
+file = open('emojicon.txt', 'r', encoding="utf8")
+emoji_lst = file.read().split('\n')
+emoji_dict = {}
+for line in emoji_lst:
+    key, value = line.split('\t')
+    emoji_dict[key] = str(value)
+file.close()
+#################
+#LOAD TEENCODE
+file = open('teencode.txt', 'r', encoding="utf8")
+teen_lst = file.read().split('\n')
+teen_dict = {}
+for line in teen_lst:
+    key, value = line.split('\t')
+    teen_dict[key] = str(value)
+file.close()
+###############
+#LOAD TRANSLATE ENGLISH -> VNMESE
+file = open('english-vnmese.txt', 'r', encoding="utf8")
+english_lst = file.read().split('\n')
+english_dict = {}
+for line in english_lst:
+    key, value = line.split('\t')
+    english_dict[key] = str(value)
+file.close()
+################
+#LOAD wrong words
+file = open('wrong-word.txt', 'r', encoding="utf8")
+wrong_lst = file.read().split('\n')
+file.close()
+#################
+#LOAD STOPWORDS
+file = open('vietnamese-stopwords.txt', 'r', encoding="utf8")
+stopwords_lst = file.read().split('\n')
+file.close()
+
+
+from Library_functions_NLP_TV import *
+
+
+
+
+# load tfidf vectorizer
+vectorizer = pickle.load(open('tfidf.pkl', 'rb'))
+
+
+# load model
+model = pickle.load(open('finalized_model_2.sav', 'rb'))
+
+
+
+# Part 2: Build app
+
+# Title
+st.image('download.jpg')
+
+st.title("Trung tâm tin học - ĐH KHTN")
+st.header('Data Science and Machine Learning Certificate')
+st.markdown('#### Project: Sentiment analysis of Vietnamese comments on Shopee')
+
+# st.video('https://www.youtube.com/watch?v=q3nSSZNOg38&list=PLFTWPHJsZXVVnckL0b3DYmHjjPiRB5mqX')
+
+menu = ['Overview', 'New Prediction']
+choice = st.sidebar.selectbox('Menu', menu)
+
+if choice == 'Overview':
+    st.subheader('Overview')
+    
+    st.write('''
+    This project is about sentiment analysis of Vietnamese comments on Shopee. 
+    The dataset is collected from Shopee website. 
+    The dataset contains 2 columns: rating and comment. 
+    The rating is from 1 to 5. 
+    The comment is the comment of customers after they buy products on Shopee. 
+    The goal of this project is to build a model to predict the sentiment of comments. 
+    The sentiment is positive or negative. 
+    ''')
+    st.write('''
+    The dataset has 2 classes: positive and negative. 
+    ''')
+    st.write('''
+    The model is built with Logistic Regression and applying oversampling data:
+    - The model has 86% accuracy.
+    - The model has 94% precision for the positive class.
+    - The model has 85% recall for the positive class.
+    - The model has 73% precision for the negative class.
+    - The model has 88% recall for the negative class. 
+    ''')
+    
+elif choice == 'New Prediction':
+    st.subheader('New Prediction')
+    st.write('''
+    Input a comment and the model will predict the sentiment of the comment. 
+    ''')
+    comment = st.text_input('Input a comment')
+    if st.button('Predict'):
+        if comment != '':
+            comment = process_text(comment, emoji_dict, teen_dict, wrong_lst)
+            comment = covert_unicode(comment)
+            comment = process_special_word(comment)
+            comment = process_postag_thesea(comment)
+            comment = remove_stopword(comment, stopwords=stopwords_lst)
+            comment = vectorizer.transform([comment])
+            sentiment = model.predict(comment)
+            if sentiment == 1:
+                st.write('The sentiment of the comment is positive')
+            else:
+                st.write('The sentiment of the comment is negative')
+        else:
+            st.write('Please input a comment')
